d978d684a6365766bfe6d29a9f7dd5d336cec34a
Muhd_Aflah <mohammedaflah60@gmail.com>
2026-01-15 17:42:15 +0530

    Fixed bugs

--- Frontend/src/sections/HeroSection.jsx
+++ Frontend/src/sections/HeroSection.jsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { QuoteModal } from "./QuoteModal";
+const navigate = useNavigate();
 
 export const HeroSection = () => {
   const [isQuoteOpen, setIsQuoteOpen] = useState(false);
