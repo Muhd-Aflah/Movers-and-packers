cf5d6c6b95aa81c827feb1638d59b1b283acc707
Muhd_Aflah <mohammedaflah60@gmail.com>
2026-01-15 19:31:55 +0530

    Fix

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,6 @@ app.use(express.json());
 
 // Routes
 app.use("/api/quotes", quoteRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api/auth", require("./src/routes/auth.routes"));
 app.use("/api/users", require("./src/routes/user.routes"));
 app.use("/api/admin", require("./src/routes/admin.routes"));
