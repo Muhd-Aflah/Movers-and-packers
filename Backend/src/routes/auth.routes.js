1e24023f766924b6de5237ebef6c29c6d5cbc025
Muhd_Aflah <mohammedaflah60@gmail.com>
2026-01-21 14:05:52 +0530

    Fixes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -3,16 +3,12 @@ const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
 const { protect } = require("../middleware/auth.middleware");
-const allowRoles = require("../middleware/role.middleware");
-const role = require("../middleware/role.middleware");
 
 // Public routes
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
-// Protected routes
+// Protected route
 router.get("/profile", protect, authController.profile);
-router.get("/check-user", protect, role("user", "admin"), authController.checkUser);
-router.get("/check-admin", protect, role("admin"), authController.checkAdmin);
 
 module.exports = router;
